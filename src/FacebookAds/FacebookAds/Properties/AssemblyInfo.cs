6b12451be6800d81f7427bb6e5f3ea9d49b4b275
Paradoxis <luke@paradoxis.nl>
2016-02-09 11:03:17 +0100

    Initial commit

---
+++ src/FacebookAds/FacebookAds/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FacebookAds")]
+[assembly: AssemblyDescription("C# Facebook Marketing SDK")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Searchresult Performancemarketing")]
+[assembly: AssemblyProduct("FacebookAds")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2bcfac71-941c-4469-a5f1-28a4f9bd1128")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
